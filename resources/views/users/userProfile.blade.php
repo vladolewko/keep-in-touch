fd362f06e35de25e5618d1a357305a0597f4591e
Vladyslav Oleshko <vladyslav.o@droam.com>
2025-10-10 19:55:02 +0300

    minor refactoring, provide services/repositories

--- resources/views/users/userProfile.blade.php
+++ resources/views/users/userProfile.blade.php
@@ -17,10 +17,4 @@
             @endif
         </div>
     </div>
-    <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            window.dataLayer.push(@json($profileGTM));
-            console.log(dataLayer);
-        });
-    </script>
 </x-app-layout>
